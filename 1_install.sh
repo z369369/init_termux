c38091696284cf857f9d5a4e164819c34d6012e2
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-08-25 07:28:44 +0900

    1

--- 1_install.sh
+++ 1_install.sh
@@ -8,6 +8,8 @@ pkg install git termux-tools ncurses-utils openssl-tool ffmpeg -y
 echo 'am start -n is.xyz.mpv/.MPVActivity "$1"' > $PREFIX/bin/mpv
 chmod +x $PREFIX/bin/mpv
 
+cd ~
+
 git clone https://github.com/CoolnsX/dra-cla
 git clone https://github.com/pystardust/ani-cli
 git clone https://github.com/z369369/init_termux
