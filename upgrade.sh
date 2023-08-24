9626aa66e69c0ec7e3ba923bb044ceb5230745b4
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-08-25 07:12:38 +0900

    upgrade

--- upgrade.sh
+++ upgrade.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-cd init_termux
-git pull
-
 cd ~
 
 cd ani-cli
 git pull
 cp -i ani-cli $PREFIX/bin/ani-cli
 
+cd ~ 
+
 cd dra-cla
 git pull
 cp -i dra-cla $PREFIX/bin/dra-cla
+
+cd ~ 
+
+cd init_termux
+git pull
