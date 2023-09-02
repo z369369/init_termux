6be49cffbb032f1af5848e1ab899920c506af3e2
ppp821203 <40662344+ppp821203@users.noreply.github.com>
2023-09-02 17:33:51 +0900

    upgrade

--- upgrade.sh
+++ upgrade.sh
@@ -3,8 +3,8 @@ cd ~
 
 git clone https://github.com/CoolnsX/dra-cla
 git clone https://github.com/pystardust/ani-cli
-git clone https://github.com/justchokingaround/jerry
-git clone https://github.com/justchokingaround/lobster
+# git clone https://github.com/justchokingaround/jerry
+# git clone https://github.com/justchokingaround/lobster
 git clone https://github.com/z369369/init_termux
 
 cd ~/ani-cli
@@ -17,15 +17,21 @@ git reset --hard HEAD
 git pull
 cp -f dra-cla $PREFIX/bin/dra-cla
 
-cd ~/jerry
-git reset --hard HEAD
-git pull
-cp -f jerry.sh $PREFIX/bin/jerry
+# cd ~/jerry
+# git reset --hard HEAD
+# git pull
+# cp -f jerry.sh $PREFIX/bin/jerry
 
-cd ~/lobster
-git reset --hard HEAD
-git pull
-cp -f lobster.sh $PREFIX/bin/lobster
+# cd ~/lobster
+# git reset --hard HEAD
+# git pull
+# cp -f lobster.sh $PREFIX/bin/lobster
+
+rm -rf $PREFIX/bin/jerry
+rm -rf $PREFIX/bin/lobster
+rm -rf ~/lobster
+rm -rf ~/jerry
+
 
 cd ~/init_termux
 git reset --hard HEAD
